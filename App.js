27c3bc0141d345455948a6d633b06fa3c1e0f929
Nikhil14311 <karanamnikhil00@gmail.com>
2022-05-23 07:30:29 -0700

    First commit in my project

---
+++ App.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View style={{flex:1,backgroundColor:'black',alignItems:'center',justifyContent:'center'}}>
+      <Text>Nikhil Karanam</Text>
+    </View>
+  )
+  
+}
+
+export default App
